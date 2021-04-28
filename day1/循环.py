36e56a9a924ceef34ff881f9ee92a2bd1896f567
gongjw <gongjw@seehoo.cn>
2021-04-28 16:27:37 +0800

    循环

--- day1/循环.py
+++ day1/循环.py
@@ -1,5 +1,23 @@
 
 
+#
+# import random
+# number =random.randint(1, 100)
+# count = 0
+# while count < 7:
+#     guess = input("请输入一个数字：")
+#     guess =int (guess)
+#     count +=1#循环一次加1，也可以写成 count =count+1
+#     if guess ==number:
+#         print("恭喜你猜对了！数字是%s" %number)
+#         break
+#     elif guess <number:
+#         print("猜小了")
+#         continue#可写可不写
+#     elif guess >number:
+#         print("猜大了")
+#         continue#可写可不写
+#         #continue如果不写，则count += 1 可写到后面
 
 import random
 number =random.randint(1, 100)
@@ -7,7 +25,7 @@ count = 0
 while count < 7:
     guess = input("请输入一个数字：")
     guess =int (guess)
-    count +=1#循环一次加1，也可以写成 count =count+1
+    count -=1#循环一次加1，也可以写成 count =count+1
     if guess ==number:
         print("恭喜你猜对了！数字是%s" %number)
         break
@@ -18,7 +36,6 @@ while count < 7:
         print("猜大了")
         continue#可写可不写
         #continue如果不写，则count += 1 可写到后面
-
 
 # +=
 # -=

82a892a3acbb606b74dda8139fb242d5a9f020a0
gongjw <gongjw@seehoo.cn>
2021-05-11 18:09:19 +0800

    qiepian

--- day3/非空即真.py
+++ day3/非空即真.py
@@ -1,1 +1,21 @@
 #非空即真，非零即真
+a =""
+b=[]
+c={}
+d=0
+f=None
+#判断username是否为空
+username=input("用户名:").strip()
+# if len(username)==0:
+#     print("输入不能为空")
+#True:
+# if username:
+#     print("用户名为：",username)
+# else:
+#     print("username不能为空")
+
+#false:
+if not username:
+    print("username不能为空")
+else:
+    print("用户名为", username)

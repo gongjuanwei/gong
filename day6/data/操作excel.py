ac9b39b8f35c79c3c1453aa31f307060b46dba3f
gongjw <gongjw@seehoo.cn>
2021-07-14 18:32:27 +0800

    excel

--- day6/data/操作excel.py
+++ day6/data/操作excel.py
@@ -2,9 +2,9 @@
 import xlwt
 book =xlwt.Workbook()
 sheet =book.add_sheet("sheet1")
-sheet.write(0,0,"id")
-sheet.write(0,0,"1")
-book.save(test.xls)#结尾用xslx的话，用office打开有问题，wps打开没问题
+sheet.write(0,1,"1")
+sheet.write(0,id,"0")
+book.save("test1.xls")#结尾用xslx的话，用office打开有问题，wps打开没问题
 
 
 
